1d65ba28da89b5028cb9be2d7b2e6580db1cc938
Song-Gi-Sung <wlswlek123@naver.com>
2023-10-25 18:24:01 +0900

    2023.10.25 redis

---
+++ src/main/java/study/market/etc/service/RedisService.java
@@ -0,0 +1,31 @@
+package study.market.etc.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+
+@Service
+@RequiredArgsConstructor
+public class RedisService {
+
+    private final StringRedisTemplate redisTemplate;
+
+    public String getData(String key) {
+
+        return redisTemplate.opsForValue().get(key);
+    }
+
+    public void setDataExpire(String key, String value, long seconds) {
+
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        Duration time = Duration.ofSeconds(seconds);
+        valueOperations.set(key, value, time);
+
+    }
+
+
+
+}
